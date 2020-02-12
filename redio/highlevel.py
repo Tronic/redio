d6ac3ac85a2ca205c3e938cfa0add037b5c0fef2
L. Kärkkäinen <tronic@users.noreply.github.com>
2020-02-12 14:04:41 +0200

    Refactor connection management. Fix bugs with auth/select. Make low level API run single command at a time and raise exceptions.

--- redio/highlevel.py
+++ redio/highlevel.py
@@ -1,16 +1,41 @@
+from __future__ import annotations
 from contextlib import contextmanager
 from functools import partial
 
 from redio import conv
 from redio.commands import CommandBase
-from redio.conn import Connection
+from redio.conn import prepare
 from redio.exc import RedisError
+from redio.protocol import Protocol
 
-class RedisDB(CommandBase):
-    """Redis database connection."""
-    def __init__(self, connector):
-        self.connector = connector
-        self.conn = connector._borrow_connection()
+
+class Redis:
+    """Redis connection pool."""
+    def __init__(self, url="redis://localhost/", *, ssl_context=None, pool_max=100):
+        self.conninfo = prepare(
+            url,
+            ssl_context=ssl_context,
+        )
+        self.pool_max = pool_max
+        self.pool = []
+
+    def __call__(self) -> DB:
+        """Get a Redis database connection."""
+        return DB(self)
+
+    def _borrow_connection(self) -> Protocol:
+        return self.pool.pop() if self.pool else Protocol(self.conninfo)
+
+    def _restore_connection(self, connection: Protocol):
+        if len(self.pool) < self.pool_max:
+            self.pool += connection,
+
+
+class DB(CommandBase):
+    """Redis database connection (high level API)."""
+    def __init__(self, redis: Redis):
+        self.redis = redis
+        self.protocol = redis._borrow_connection()
         self.commands = []
         self.bytedecode = None
 
@@ -18,13 +43,13 @@ class RedisDB(CommandBase):
         """Restore still usable connection to pool on garbage collect. We rely
         partially on CPython's reference counting but also note that it is not
         crucial for connections to be returned immediately."""
-        if self.connector and self.conn:
-            self.connector._restore_connection(self.conn)
+        if self.redis and not self.protocol.closed:
+            self.redis._restore_connection(self.protocol)
 
     @property
     def prevent_pooling(self):
         """Prevent this connection being returned to connection pool."""
-        self.connector = None
+        self.redis = None
         return self
 
     def __await__(self):
@@ -64,16 +89,16 @@ class RedisDB(CommandBase):
 
     async def _run(self):
         """Execute queued commands, equivalent to await self."""
-        if not self.conn:
-            await self.conn.connect()
+        if self.protocol.closed:
+            await self.protocol.connect()
         try:
             if self.commands:
                 return await self._execute()
         except:
             # Any error and we assume that the connection is in invalid state.
             self.prevent_pooling
-            await self.conn.aclose()
-            self.conn = None
+            await self.protocol.aclose()
+            self.protocol = None
             raise
 
     async def _execute(self):
@@ -84,7 +109,7 @@ class RedisDB(CommandBase):
             handlers.append(handler)
             commands.append([conv.encode(a) for a in cmd])
         self.commands = []
-        res = await self.conn.run(commands)
+        res = await self.protocol.run(commands)
         ret = []
         for h, r in zip(handlers, res):
             if h is None:
@@ -103,22 +128,3 @@ class RedisDB(CommandBase):
     def _command(self, *cmd, handler=None):
         self.commands.append((handler, cmd))
         return self
-
-
-class Redis:
-    """Redis connection pool."""
-    def __init__(self, url="redis://localhost/", *, ssl_context=None, pool_max=100):
-        self.new = partial(Connection, url, ssl_context=ssl_context)
-        self.pool_max = pool_max
-        self.pool = [self.new()]  # Mainly to test that args are OK!
-
-    def __call__(self) -> RedisDB:
-        """Get a Redis database connection."""
-        return RedisDB(self)
-
-    def _borrow_connection(self) -> Connection:
-        return self.pool.pop() if self.pool else self.new()
-
-    def _restore_connection(self, connection: Connection):
-        if len(self.pool) < self.pool_max:
-            self.pool += connection,
--- redio/protocol.py
+++ redio/protocol.py
@@ -2,18 +2,93 @@ import json
 
 from functools import partial
 
+from trio.abc import Stream
+
 from redio import conv
+from redio.conn import ConnectInfo
 from redio.exc import ProtocolError, ServerError
 
 class Protocol:
-    def __init__(self):
+    """Redis protocol (server connection low level API)."""
+    def __init__(self, conninfo: ConnectInfo):
         self.inbuf = bytearray()
         self.outbuf = bytearray()
+        self.conninfo = conninfo
+        self.sock = None
 
-    def data_to_send(self):
-        ret = bytes(self.outbuf)
-        del self.outbuf[:]
+    @property
+    def closed(self):
+        return self.sock is None
+
+    async def connect(self):
+        self.sock = await self.conninfo.socket_connect()
+        if self.conninfo.password:
+            await self.AUTH(self.conninfo.password.encode())
+        if self.conninfo.database:
+            await self.SELECT(b"%d" % self.conninfo.database)
+
+    async def aclose(self):
+        """Close connection."""
+        if not self.closed:
+            del self.inbuf[:], self.outbuf[:]
+            sock, self.sock = self.sock, None
+            await sock.aclose()
+
+    def __getattr__(self, name):
+        """Allow calling protocol.PING() etc. to run commands."""
+        if name.isupper():
+            setattr(self, name, partial(self.run_single, name.encode()))
+        return super().__getattribute__(name)
+
+    def _command(self, cmd: tuple):
+        """Queue a single command for execution."""
+        buffer = self.outbuf
+        buffer += b"*%d\r\n" % len(cmd)
+        for a in cmd:
+            buffer += b"$%d\r\n%b\r\n" % (len(a), a)
+        return self
+
+    async def run_single(self, *cmd):
+        ret, = await self.run([cmd])
+        if isinstance(ret, ServerError): raise ret
         return ret
+
+    async def run(self, commands):
+        """Run a set of commands and return responses.
+
+        Uses pipelining but no transactions. Each command must return exactly
+        one response. For commands where this is not true, Protocol must be
+        accessed directly.
+
+        RedisErrors are returned in the array rather than raised.
+        """
+        if self.closed:
+            raise ValueError("Attempting to run on a closed connection")
+        try:
+            for cmd in commands:
+                self._command(cmd)
+            if self.inbuf:
+                raise ProtocolError(
+                    f"Pipelining error: previous bytes unread: {self.inbuf[:10000]}")
+            if self.sock.socket.is_readable():
+                raise ProtocolError(
+                    f"Pipelining error: server sent unexpected data"
+                )
+            await self.send_all()
+            ret = [await self.receive() for _ in commands]
+            if self.inbuf:
+                raise ProtocolError(
+                    f"Pipelining error: bytes left unread: {self.inbuf[:10000]}")
+            return ret
+        except:
+            await self.aclose()
+            raise
+
+    async def send_all(self):
+        """Send any queued commands to server."""
+        if self.outbuf:
+            await self.sock.send_all(self.outbuf)
+            del self.outbuf[:]
 
     async def receive(self):
         t, arg = await self._recvline()
@@ -30,7 +105,7 @@ class Protocol:
         while True:
             pos = buffer.find(b"\r\n")
             if pos != -1: break
-            buffer += await self.receiver()
+            buffer += await self.sock.receive_some()
         ret = buffer[:pos].decode()
         del buffer[:pos + 2]
         return ret[0], ret[1:]
@@ -38,22 +113,9 @@ class Protocol:
     async def _recvbulk(self, num):
         buffer = self.inbuf
         while len(buffer) < num + 2:
-            buffer += await self.receiver()
+            buffer += await self.sock.receive_some()
         if buffer[num:num+2] != b"\r\n":
             raise ProtocolError(f"Redis protocol out of sync (no CRLF after bulk)")
         ret = buffer[:num]
         del buffer[:num+2]
         return bytes(ret)
-
-    def command(self, cmd: tuple):
-        buffer = self.outbuf
-        buffer += b"*%d\r\n" % len(cmd)
-        for a in cmd:
-            buffer += b"$%d\r\n%b\r\n" % (len(a), a)
-        return self
-
-    def __getattr__(self, name):
-        """Allow calling protocol.PING() etc. to queue commands."""
-        if name.isupper():
-            setattr(self, name, partial(self._command, name.encode()))
-        return super().__getattribute__(name)
