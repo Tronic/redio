1ad5f12dd59a2dc9a486c9998bac371862fe6677
L. Kärkkäinen <98187+Tronic@users.noreply.github.com>
2022-07-31 20:37:10 +0300

    Release 1.0.0, mark stable.

--- setup.py
+++ setup.py
@@ -15,11 +15,13 @@ setup(
         "Redis", "key-value store", "trio", "async", "database", "networking"
     ],
     classifiers = [
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: System :: Networking",
         "Framework :: Trio",
         "License :: OSI Approved :: MIT License",
